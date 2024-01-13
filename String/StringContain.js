70e8d8c2e0a52089932ab198ed12686e57937b0f
Spirit <455814193@qq.com>
2024-01-13 23:34:16 +0800

    一个字符串是否是另一个字符串的子集

---
+++ String/StringContain.js
@@ -0,0 +1,25 @@
+/*
+ * @Description: 一个字符串是否是另一个字符串的子集
+ * @Author: Qinaj
+ * @Date: 2024-01-13 22:36:03
+ * @LastEditTime: 2024-01-13 23:31:32
+ * @LastEditors: Qinaj
+ */
+function stringContains(orgStr = '', str = ''){
+   if(!str?.length) return false
+   let i = 0
+   let arr = orgStr.split('')
+   while(i >=0 && i<str.length){
+      let curIndex = arr.findIndex(item => item === str[i])
+      if(curIndex > -1){
+         arr.splice(curIndex, 1)
+         i += 1
+      }else{
+        return false
+      }
+   }
+   return true
+   
+}
+//删除之后反复更新orgStr参数 可以防止顺序错乱问题 因为删除就会更新原值
+console.log(stringContains('haha哈哈哈23','haha呵哈哈哈23'))
